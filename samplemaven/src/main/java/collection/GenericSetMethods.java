1c39fae2ac695a6594b8eca8f171a15d17bb1f99
ASUS <ASUS@Anns-PC>
2025-12-05 10:12:40 +0400

    5 December 2025

--- samplemaven/src/main/java/collection/GenericSetMethods.java
+++ samplemaven/src/main/java/collection/GenericSetMethods.java
@@ -6,10 +6,25 @@ import java.util.Set;
 public class GenericSetMethods {
 
 	public static void main(String[] args) {
-		Set <String> a =new HashSet <String>();
+		Set <String> s =new HashSet <String>();
 
 		//add()
-		s.add
+		s.add("Green");
+		s.add("Violet");
+		s.add("Indigo");
+		s.add("Red");
+		System.out.println(s);
+		
+		Set <String> a =new HashSet <String>();
+		a.add("green");
+		a.add("Blue");
+		a.add("Orange");
+		a.add("Yellow");
+		System.out.println(a);
+		
+		//addAll()
+		s.addAll(a);
+		System.out.println(s);
 
 	}
 
